76e295bac7e4ff2fa1d5f7fb26a712f22839d6a7
Sahil Acharya <98463508+Acrsahil@users.noreply.github.com>
2025-01-09 22:31:05 +0545

    Delete alias.sh

--- alias.sh
+++
@@ -1,2 +0,0 @@
-alias gmkdir='source /home/window/Auto-Git/myenv/bin/activate && pwd | python /home/window/Auto-Git/getpath.py && python /home/window/Auto-Git/main.py'
-alias grmdir='source /home/window/Auto-Git/myenv/bin/activate && python /home/window/Auto-Git/deleterepo.py'
