10726f9079b62f0cfc7877cefa32a1efc3fe69ba
Acrsahil <acharyasahil2003@gmail.com>
2025-01-10 18:50:56 +0545

    beautifying code for better user expierence

--- get_repo.py
+++ get_repo.py
@@ -5,9 +5,26 @@ from github import Auth
 
 lsts = []
 
+maindir = os.path.dirname(os.path.abspath(__file__))
+
+# Read the token from the file
+try:
+    with open(f"{maindir}/mykey.txt", "r") as file:
+        key = file.read().strip()
+except FileNotFoundError:
+    print("Error: Token file 'mykey.txt' not found.")
+    sys.exit(1)
+
+auth = Auth.Token(key)
+g = Github(auth=auth)
+user = g.get_user()
+
 
 def ls_inside_repo(lsts):
     for con in lsts:
+        print()
+        print(f"Listing contents of {con} repo:")
+        print()
         mycontent = g.get_user().get_repo(con)
         contents = mycontent.get_contents("")
         while contents:
@@ -20,24 +37,16 @@ def ls_inside_repo(lsts):
 
 def ls_repos():
     myrepos = g.get_user().get_repos()
+    print()
+    print(f"Listing repo of {user.login}")
+    print()
     for repo in myrepos:
         print(repo.name)
 
 
 # Get the directory of the current script
-maindir = os.path.dirname(os.path.abspath(__file__))
-
-# Read the token from the file
-try:
-    with open(f"{maindir}/mykey.txt", "r") as file:
-        key = file.read().strip()
-except FileNotFoundError:
-    print("Error: Token file 'mykey.txt' not found.")
-    sys.exit(1)
 
 # Authenticate with GitHub
-auth = Auth.Token(key)
-g = Github(auth=auth)
 
 
 lsts = sys.argv[1:]
